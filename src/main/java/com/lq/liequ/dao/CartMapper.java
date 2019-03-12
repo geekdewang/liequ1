b8a4e355d261693e8c853f26f6cb1b43bf156a49
lvmingyang <lvmingyang77@163.com>
2019-03-12 21:33:54 +0800

    购物车逆向生成

---
+++ src/main/java/com/lq/liequ/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.lq.liequ.dao;
+
+import com.lq.liequ.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer cid);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer cid);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/lq/liequ/pojo/Cart.java
@@ -0,0 +1,43 @@
+package com.lq.liequ.pojo;
+
+public class Cart {
+    private Integer cid;
+
+    private String cname;
+
+    private Double price;
+
+    private Integer num;
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname == null ? null : cname.trim();
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
