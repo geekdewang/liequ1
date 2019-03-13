8bfda7a65adb7d1499f4912ce3b55f578f82acd5
geekdewang <geekdewang@163.com>
2019-03-13 19:36:13 +0800

    购物车改动

--- src/main/java/com/lq/liequ/Controller/CartController.java
+++ src/main/java/com/lq/liequ/Controller/CartController.java
@@ -3,21 +3,28 @@ package com.lq.liequ.Controller;
 import com.lq.liequ.common.vo.ResultVo;
 import com.lq.liequ.pojo.Cart;
 import com.lq.liequ.service.CartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@Api(value = "购物车",tags = "购物车")
 public class CartController {
 
     @Autowired
     private CartService cartService;
 
-    @RequestMapping("cartadd")
+    @ApiOperation(value = "购物车的添加",notes = "把名字和价钱传递过来添加")
+    @PostMapping("cartadd")
     public ResultVo add(Cart cart) {
-
         return cartService.add(cart);
+    }
+
+    @ApiOperation(value = "购物车的展示")
+    @GetMapping("cartlist")
+    public ResultVo cartlist() {
+        return cartService.cartlist();
     }
 }
